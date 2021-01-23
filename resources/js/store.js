03e52809bc64d1167b8670881227d8900ebce521
Amir Rehman <aamirnorth@gmail.com>
2021-01-23 15:40:04 +0400

    23 January 2021 2

--- resources/js/store.js
+++ resources/js/store.js
@@ -12,8 +12,8 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
 
     state: {
-        publicUrl: "http://127.0.0.1:8000/storage",
-        baseUrl: "http://127.0.0.1:8000",
+        publicUrl: "https://gyporomo.com/storage",
+        baseUrl: "https://gyporomo.com",
         isModalOne: false,
         isBroucherModal: false,
         isSpecSheetModal: false,
