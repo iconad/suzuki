fd90a5c379d7caecf572f57ffaa966ff08a176f5
Amir Rehman <aamirnorth@gmail.com>
2021-02-18 15:34:51 +0500

    MSI update v4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Suzuki') }}</title>
+    <title>Suzuki UAE</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
